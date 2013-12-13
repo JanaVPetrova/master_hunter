e84797255b6495cfb84ae8b29d4767dc7356c44d
Jana Petrova <janavpetrova@gmail.com>
2013-12-13 21:46:50 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MasterHunter::Application.config.secret_key_base = '080938518d7f7c8272ccf2115eb2e2b9a1b8b7aed3c7478fbfe97314b7db8771c27df996c93a79c603bfd349c99d34eeaea80610089d40627d8a8ad9525ccfe7'
