654e94b9951d91e8b1267f28821031b769b0c7a8
pavel <kalashnikovisme@gmail.com>
2013-12-14 16:34:30 +0400

    add locales

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,15 @@ Bundler.require(:default, Rails.env)
 
 module MasterHunter
   class Application < Rails::Application
+    config.encoding = "utf-8"
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.filter_parameters += [:password]
+    config.active_support.escape_html_entities_in_json = true
+    config.active_record.whitelist_attributes = true
+    config.assets.enabled = true
+    config.assets.version = '1.0'
+    config.i18n.default_locale = :ru
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.generators do |g|
       g.template_engine :haml
       g.helper false
