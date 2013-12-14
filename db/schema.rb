45dc872cf316bd3e11d72754c07f203b0c5e277b
pavel <kalashnikovisme@gmail.com>
2013-12-15 00:34:43 +0400

    add destroy button to posts

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20131214192820) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "post_photos", force: true do |t|
+    t.string   "file"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "posts", force: true do |t|
     t.string   "nickname"
     t.text     "description"
