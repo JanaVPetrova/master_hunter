07f020a9f11bed3145e753d36d5d488055c08ae1
Jana Petrova <janavpetrova@gmail.com>
2013-12-15 03:24:42 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "vencarbon@yandex.ru", password: "123456789qwerty", name: "admin", admin: true)
